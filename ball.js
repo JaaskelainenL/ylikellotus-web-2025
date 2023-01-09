676879a57620ddd25056f3d39e73ba0771f5abfc
Otto Söderman <ottoj.soderman@gmail.com>
2023-01-09 14:49:44 +0200

    Removed useless files, switched to canvas, added .prettierrc

--- ball.js
+++ ball.js
@@ -1,111 +1,114 @@
 function isOnMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  );
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 }
-const ball = document.getElementById("ball");
-const ballStyle = ball.style;
-height = document.getElementById("body").offsetHeight;
-width = document.body.clientWidth;
-const radius = ball.offsetWidth / 2;
-const isMobile = isOnMobile();
-let touchX;
-let touchY;
-let speedX;
-let speedY;
-let x = width / 2 - radius;
-let y = 100;
-let dx = 0;
-let dy = 0;
-let rotation = 0;
-let hasStarted = false;
-let lastTouchTime = new Date();
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+height = document.getElementById('body').offsetHeight
+width = document.body.clientWidth
+const ballSize = 200
+const radius = 100
+const isMobile = isOnMobile()
+const ballImage = new Image()
+ballImage.src = 'ball.png'
+let touchX
+let touchY
+let speedX
+let speedY
+let x = width / 2 - radius
+let y = 100
+let dx = 0
+let dy = 0
+let rotation = 0
+let hasStarted = false
+let lastTouchTime = new Date()
 const calculateSpeed = (touchPoint, centerPoint) => {
-  const distanceFromCenter = touchPoint - centerPoint - radius;
-  const normalizedDistance = distanceFromCenter / radius;
-  const speed = normalizedDistance * 10;
-  return speed;
-};
+  const distanceFromCenter = touchPoint - centerPoint - radius
+  const normalizedDistance = distanceFromCenter / radius
+  const speed = normalizedDistance * 10
+  return speed
+}
 function handleTouch(e) {
-  if (
-    e.type == "touchstart" ||
-    e.type == "touchmove" ||
-    e.type == "touchend" ||
-    e.type == "touchcancel"
-  ) {
-    const evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
-    const touch = evt.touches[0] || evt.changedTouches[0];
-    touchX = touch.pageX;
-    touchY = touch.pageY;
-    speedX = calculateSpeed(touch.pageX, x);
-    speedY = -calculateSpeed(touch.pageY, y);
-  } else if (e.type === "mousemove") {
-    touchX = e.pageX;
-    touchY = e.pageY;
-    const mouseSpeedMultiplier = 0.5;
-    speedX = e.movementX * mouseSpeedMultiplier;
-    speedY = e.movementY * mouseSpeedMultiplier;
+  if (e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel') {
+    const evt = typeof e.originalEvent === 'undefined' ? e : e.originalEvent
+    const touch = evt.touches[0] || evt.changedTouches[0]
+    touchX = touch.pageX
+    touchY = touch.pageY
+    speedX = calculateSpeed(touch.pageX, x)
+    speedY = -calculateSpeed(touch.pageY, y)
+  } else if (e.type === 'mousemove') {
+    touchX = e.pageX
+    touchY = e.pageY
+    const mouseSpeedMultiplier = 0.5
+    speedX = e.movementX * mouseSpeedMultiplier
+    speedY = e.movementY * mouseSpeedMultiplier
   }
-  const isValid = touchX && touchY && speedX && speedY;
-  const isInBall =
-    touchX > x &&
-    touchX < x + 2 * radius &&
-    touchY > y &&
-    touchY < y + 2 * radius;
-  const timeNow = new Date();
-  const timeSinceLastTouch = timeNow - lastTouchTime;
-  const isSomeTimeAfterLastTouch = timeSinceLastTouch > 100;
+  const isValid = touchX && touchY && speedX && speedY
+  const isInBall = touchX > x && touchX < x + 2 * radius && touchY > y && touchY < y + 2 * radius
+  const timeNow = new Date()
+  const timeSinceLastTouch = timeNow - lastTouchTime
+  const isSomeTimeAfterLastTouch = timeSinceLastTouch > 100
   if (isValid && isInBall && isSomeTimeAfterLastTouch) {
-    lastTouchTime = timeNow;
-    const speedMultiplier = 2;
-    dx = speedX * speedMultiplier;
-    dy = speedY * speedMultiplier;
+    lastTouchTime = timeNow
+    const speedMultiplier = 2
+    dx = speedX * speedMultiplier
+    dy = speedY * speedMultiplier
   }
 }
-const el = document.getElementById("body");
-el.addEventListener("touchmove", handleTouch);
-el.addEventListener("touchstart", handleTouch);
-el.addEventListener("touchend", handleTouch);
-el.addEventListener("touchcancel", handleTouch);
-el.addEventListener("mousemove", handleTouch);
+const el = document.getElementById('body')
+el.addEventListener('touchmove', handleTouch)
+el.addEventListener('touchstart', handleTouch)
+el.addEventListener('touchend', handleTouch)
+el.addEventListener('touchcancel', handleTouch)
+el.addEventListener('mousemove', handleTouch)
+
+function drawRotatedBall(x, y, w, h, degrees) {
+  ctx.save()
+  ctx.translate(x + w / 2, y + h / 2)
+  ctx.rotate((degrees * Math.PI) / 180.0)
+  ctx.translate(-x - w / 2, -y - h / 2)
+  ctx.drawImage(ballImage, x, y, w, h)
+  ctx.restore()
+}
+
 function update() {
-  height = document.getElementById("body").offsetHeight;
-  width = document.body.clientWidth;
-  const slowdownRate = 100;
-  dx -= dx / slowdownRate;
-  dy -= dy / slowdownRate;
-  const gravity = hasStarted ? 0.05 : 0;
-  dy += gravity;
-  x += dx;
-  y += dy;
-  const maxX = width - 2 * radius;
-  const maxY = height - 2 * radius;
+  canvas.width = width
+  canvas.height = height
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  height = document.getElementById('body').offsetHeight
+  width = document.body.clientWidth
+  const slowdownRate = 100
+  dx -= dx / slowdownRate
+  dy -= dy / slowdownRate
+  const gravity = hasStarted ? 0.05 : 0
+  dy += gravity
+  x += dx
+  y += dy
+  const maxX = width - 2 * radius
+  const maxY = height - 2 * radius
   if (x > maxX) {
-    x = maxX;
-    dx = -dx;
+    x = maxX
+    dx = -dx
   }
   if (y > maxY) {
-    y = maxY;
-    dy = -dy;
+    y = maxY
+    dy = -dy
   }
   if (x < 0) {
-    x = 0;
-    dx = -dx;
+    x = 0
+    dx = -dx
   }
   if (y < 0) {
-    y = 0;
-    dy = -dy;
+    y = 0
+    dy = -dy
   }
-  ballStyle.left = `${x}px`;
-  ballStyle.top = `${y}px`;
-  const minRotation = y + 20 >= maxY || !hasStarted ? 0 : 2;
-  rotation += Math.max(Math.abs(dx), Math.abs(dy), minRotation);
-  ballStyle.transform = `rotate(${rotation}deg)`;
-  window.requestAnimationFrame(update);
+  const minRotation = y + 20 >= maxY || !hasStarted ? 0 : 2
+  rotation += Math.max(Math.abs(dx), Math.abs(dy), minRotation)
+  drawRotatedBall(x, y, ballSize, ballSize, rotation)
+  window.requestAnimationFrame(update)
 }
-const timeToAnimationStart = 3000;
+const timeToAnimationStart = 3000
 setTimeout(function () {
-  hasStarted = true;
-}, timeToAnimationStart);
+  hasStarted = true
+}, timeToAnimationStart)
 
-window.requestAnimationFrame(update);
+window.requestAnimationFrame(update)
--- countdown.js
+++ countdown.js
@@ -1,19 +1,15 @@
-const eventDate = new Date("2023-02-15T18:00:00+02:00");
+const eventDate = new Date('2023-02-15T18:00:00+02:00')
 const updateCountdown = () => {
-  const timeLeft = eventDate - new Date();
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-  document.getElementById(
-    "counter"
-  ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-};
-updateCountdown();
+  const timeLeft = eventDate - new Date()
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
+  document.getElementById('counter').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`
+}
+updateCountdown()
 
 setInterval(() => {
-  updateCountdown();
-}, 1000);
-document.h;
+  updateCountdown()
+}, 1000)
+document.h
