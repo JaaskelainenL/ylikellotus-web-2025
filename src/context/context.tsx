0ba11c4b3b2bf6813d20d248cdf8a280376a78c7
Toni Tertsonen <toni.tertsonen@aalto.fi>
2024-01-26 01:20:31 +0200

    refactor: <div/> instead of React.createElement

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,5 +1,4 @@
 import { createContext } from "react";
 import { localizedTexts } from "../Localization";
-import React from "react";
 
-export const LocalizationContext = createContext<(name: keyof typeof localizedTexts) => JSX.Element>(() => /* Why is </div> not working????? */ React.createElement("div"))
+export const LocalizationContext = createContext<(name: keyof typeof localizedTexts) => JSX.Element>(() => <div/>)
