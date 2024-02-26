a64a03e106344c115e62666f5e9c39bb92e9cd4d
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2024-02-26 18:25:05 +0200

    feat: finish leaderboard admin basic functionality

--- src/pages/LeaderboardAdmin.tsx
+++ src/pages/LeaderboardAdmin.tsx
@@ -3,70 +3,86 @@ import { useState } from 'react'
 
 
 function LeaderboardAdmin() {
-    const [inputs, setInputs] = useState([{ name: "", guild: "", time: 0 }]);
-    const handleAddInput = () => {
-        setInputs([...inputs, { name: "", guild: "", time: 0 }]);
-    };
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
-        let { name, value } = event.target;
-        let onChangeValue: {
-            [key: string]: string | number;
-            name: string;
-            guild: string;
-            time: number;
-        }[] = [...inputs];
-        onChangeValue[index][name] = value;
-        setInputs(onChangeValue);
-    };
-    const handleDeleteInput = (index: number) => {
-        const newArray = [...inputs];
-        newArray.splice(index, 1);
-        setInputs(newArray);
+    type Score = {
+        name: string;
+        guild: string;
+        time: number;
     };
 
-    const postData = () => {
+    const [scores, setScores] = useState<Score[]>([{ name: "", guild: "", time: 0 }]);
+    const [authHash, setAuthHash] = useState<string>("");
+
+    const handleAddScore = () => {
+        setScores([...scores, { name: "", guild: "", time: 0 }]);
+    };
+    const handleScoreChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+        const { name, value } = event.target;
+        const onChangeValue = [...scores];
+        const newValue = name === 'time' ? parseInt(value) : value;
+        onChangeValue[index] = { ...onChangeValue[index], [name]: newValue };
+        setScores(onChangeValue);
+    };
+    const handleDeleteScore = (index: number) => {
+        const newArray = [...scores];
+        newArray.splice(index, 1);
+        setScores(newArray);
+    };
+
+    // definitely secure authentication
+    const enterCredentials = () => {
+        const user = prompt("Enter username:");
+        if(user == null) {
+            return;
+        }
+        const pwd = prompt("Enter password:");
+        setAuthHash(btoa(user + ':' + pwd));
+    }
+
+    const postScores = () => {
         const requestOptions = {
             method: 'POST',
-            headers: { 
-                'Content-Type': 'application/json',
-                'Authorization': 'Basic ' + btoa("ylikellotus:password"),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic " + authHash,
             },
-            body: JSON.stringify(inputs)
+            body: JSON.stringify(scores)
         };
-        fetch('http://localhost:3000', requestOptions)
+        fetch('https://ylikellotus.lajp.fi', requestOptions)
             .then(res => res.json())
-            .then(() => setInputs([{name: "", guild: "", time: 0}]))
+            .then(() => setScores([{name: "", guild: "", time: 0}]))
+            .catch((err) => alert("Error posting, try changing credentials. Full error: " + err));
     }
 
     return (
-        <div className="container">
-            {inputs.map((item, index) => (
+        <div className="container admin">
+            {scores.map((item, index) => (
                 <div key={index}>
                     <div>
                         <input
                             name="name"
                             type="text"
                             value={item.name}
-                            onChange={(event) => handleChange(event, index)}
+                            onChange={(event) => handleScoreChange(event, index)}
                         />
                         <input
                             name="guild"
                             type="text"
                             value={item.guild}
-                            onChange={(event) => handleChange(event, index)}
+                            onChange={(event) => handleScoreChange(event, index)}
                         />
                         <input
                             name="time"
                             type="number"
                             value={item.time}
-                            onChange={(event) => handleChange(event, index)}
+                            onChange={(event) => handleScoreChange(event, index)}
                         />
-                        <button disabled={inputs.length <= 1} onClick={() => handleDeleteInput(index)}>Delete</button>
+                        <button disabled={scores.length <= 1} onClick={() => handleDeleteScore(index)}>Delete</button>
                     </div>
                 </div>
             ))}
-            <button onClick={() => handleAddInput()}>Add</button>
-            <button onClick={() => postData()}>Post</button>
+            <button onClick={handleAddScore}>Add clocker</button>
+            <button onClick={postScores}>Send</button>
+            <button onClick={enterCredentials}>Change credentials</button>
         </div>
     );
 }
