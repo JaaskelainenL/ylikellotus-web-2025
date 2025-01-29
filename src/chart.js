34ed4e95bc0bd1a9c95e86ff96aa611696dba5e2
jaaskelainenL <jaaskelainenL@protonmail.com>
2025-01-29 21:46:13 +0200

    Add chart for results Currently untested due to CORS and there not being a backend for this year TODO: Admin panel (shouldnt take too long)

---
+++ src/chart.js
@@ -0,0 +1,89 @@
+const getScores = () => {
+    fetch("https://ylikellotus.lajp.fi")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to fetch leaderboard data");
+      }
+      return res.json();
+    })
+    .then((res) => {
+      setTableData(res);
+      setGuilds(grouped(res));
+    })
+    .catch((error) => {
+      console.error("ERROR WITH FETCHING LEADERBOARD DATA: " + error);
+    });
+  setTimeout(getScores, 10000);
+}
+
+const grouped = (data) => {
+    let result= []
+    data.forEach((s) => {
+      const guild = s.guild.toUpperCase().replace('TIK', 'TiK').replace('ATHENE', 'Athene').replace('PRODEKO', 'Prodeko').replace('INKUBIO', 'Inkubio');
+      const g = result.find(g => g.name === guild)
+      if(g === undefined) {
+        result.push({name: guild, scores: [s], average: 0, median: 0, total: 0})
+      } else {
+        g.scores.push(s)
+      }
+    })
+    result.forEach((g) => {
+      g.average = g.scores.reduce((acc, c) => acc + c.time, 0) / g.scores.length;
+      g.median = g.scores[Math.floor(g.scores.length / 2)].time;
+      g.total = g.scores.length;
+    })
+    return result;
+
+
+
+}
+
+
+const setTableData = (data) => {
+    if (data === undefined || data.length == 0)
+        return;
+
+    const container = document.getElementById("results");
+    container.innerHTML = "";
+
+    data.forEach(d => {
+
+        container.innerHTML += ```
+				<div class="flex justify-between text-white text-2xl font-airstrike w-full">
+					<h1>${d.name}</h1>
+					<h1>${d.guild}</h1>
+					<h1>${d.time/1000}s</h1>
+				</div>        
+        ```
+
+    })
+
+}
+
+const setGuilds = (data) => {
+    if (data === undefined || data.length == 0)
+        return;
+
+    const container = document.getElementById("results-guild");
+    container.innerHTML = "";
+
+    data.forEach(d => {
+
+        container.innerHTML += ```
+				<div class="flex justify-between text-white text-2xl font-airstrike w-full">
+					<h1>${d.guild}</h1>
+                    <h1>${d.total}</h1>
+					<h1>${d.average/1000}s</h1>
+                    <h1>${d.median/1000}</h1>
+				</div>        
+        ```
+
+    })
+
+}
+
+
+
+
+
+getScores()
